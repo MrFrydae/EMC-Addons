e5461ce9f72eeddd7e4c2a68b1dc0c61cdb90aaa
MrFrydae <mrfrydae@gmail.com>
2024-12-08 12:10:05 -0500

    Initial Commit

---
+++ src/client/java/dev/frydae/emcaddons/EMCAddonsClient.java
@@ -0,0 +1,10 @@
+package dev.frydae.emcaddons;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EMCAddonsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
